f4f505a37516c5325cfcfb799a4385b98b169cad
FarhanSiam042 <farhanshahriar942@gmail.com>
2025-03-26 16:53:10 +0600

    Added CustomerId.java

---
+++ src/CustomerId.java
@@ -0,0 +1,9 @@
+// New file: CustomerId.java
+public record CustomerId(String value) {
+    public CustomerId {
+        if (value == null || value.length() != 6) {
+            throw new IllegalArgumentException("Invalid customer ID format");
+        }
+    }
+}
+
